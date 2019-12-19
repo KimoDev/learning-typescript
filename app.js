b7634e3e964c687a593f040bac5bcc2b1cecb646
Kimo <jm865@kent.ac.uk>
2019-12-19 11:47:51 +0000

    array & tuple types

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
+// This is not a JS object. 
+// In fact this is an object type.
+// Instead of key-value pairs like in a js object, we have key-type pairs
+// const person: {
+//   name: string;
+//   age: number
+// } = {
+//   name: 'Kimo',
+//   age: 100
+// };
+// Let typescript infer the types instead
 var person = {
     name: 'Kimo',
-    age: 100
+    age: 100,
+    hobbies: ['Programming', 'Photography', 'Creation'],
+    role: [2, 'author'] // tuple - fixed array of size 2 elements. TypeScript infers this as a UNION TYPE.
 };
-console.log(person);
+// Typescript wont catch this as an error at compilation time. (Possibly breaking)
+// person.role.push('admin');
+// Typescript will catch errors on the following lines
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
+var favouriteLanguages;
+favouriteLanguages = ['JavaScript', 'Java', 'TypeScript!'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase()); // type inference of hobby so we can called string methods without errors
+    // console.log(hobby.map()); // ERROR: cannot call map() on a string value. Typescript infers the type and makes this check.
+}
--- app.ts
+++ app.ts
@@ -11,10 +11,31 @@
 // };
 
 // Let typescript infer the types instead
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // tuple type
+} = {
     name: 'Kimo',
-    age: 100
+    age: 100,
+    hobbies: ['Programming', 'Photography', 'Creation'],
+    role: [2, 'author'] // tuple - fixed array of size 2 elements. TypeScript infers this as a UNION TYPE.
   };
 
+  // Typescript wont catch this as an error at compilation time. (Possibly breaking)
+  // person.role.push('admin');
 
-console.log(person.name);
+  // Typescript will catch errors on the following lines
+  // person.role[1] = 10;
+  // person.role = [0, 'admin', 'user'];
+  
+  let favouriteLanguages: string[]; 
+  favouriteLanguages = ['JavaScript', 'Java', 'TypeScript!']
+
+  console.log(person.name);
+
+  for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase()); // type inference of hobby so we can called string methods without errors
+    // console.log(hobby.map()); // ERROR: cannot call map() on a string value. Typescript infers the type and makes this check.
+  }
