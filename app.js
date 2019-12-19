cbbe3b54f0c747793f45da85a00ab3464b6000d6
Kimo <jm865@kent.ac.uk>
2019-12-19 12:25:26 +0000

    Added union types

--- app.js
+++ app.js
@@ -1,51 +1,15 @@
-// This is not a JS object. 
-// In fact this is an object type.
-// Instead of key-value pairs like in a js object, we have key-type pairs
-// const person: {
-//   name: string;
-//   age: number
-// } = {
-//   name: 'Kimo',
-//   age: 100
-// };
-// Let typescript infer the types instead
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]; // tuple type
-// } = {
-//     name: 'Kimo',
-//     age: 100,
-//     hobbies: ['Programming', 'Photography', 'Creation'],
-//     role: [2, 'author'] // tuple - fixed array of size 2 elements. TypeScript infers this as a UNION TYPE.
-//   };
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Kimo',
-    age: 100,
-    hobbies: ['Programming', 'Photography', 'Creation'],
-    role: Role.ADMIN
+// | symbolises union types
+var combine = function (input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 };
-// Typescript wont catch this as an error at compilation time. (Possibly breaking)
-// person.role.push('admin');
-// Typescript will catch errors on the following lines
-// person.role[1] = 10;
-// person.role = [0, 'admin', 'user'];
-var favouriteLanguages;
-favouriteLanguages = ['JavaScript', 'Java', 'TypeScript!'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase()); // type inference of hobby so we can called string methods without errors
-    // console.log(hobby.map()); // ERROR: cannot call map() on a string value. Typescript infers the type and makes this check.
-}
-if (person.role === Role.ADMIN) {
-    console.log('user is logged in as a Admin');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('joseph', 'kimo');
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,63 +1,16 @@
-// This is not a JS object. 
-// In fact this is an object type.
-// Instead of key-value pairs like in a js object, we have key-type pairs
-
-// const person: {
-//   name: string;
-//   age: number
-// } = {
-//   name: 'Kimo',
-//   age: 100
-// };
-
-// Let typescript infer the types instead
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]; // tuple type
-// } = {
-//     name: 'Kimo',
-//     age: 100,
-//     hobbies: ['Programming', 'Photography', 'Creation'],
-//     role: [2, 'author'] // tuple - fixed array of size 2 elements. TypeScript infers this as a UNION TYPE.
-//   };
-
-  // enum starting from 5. Can also assign custom values with each enum CONSTANT. 
-  enum Role { ADMIN = 5, READ_ONLY = 100, AUTHOR = 'Author'};
-
-  const person = {
-    name: 'Kimo',
-    age: 100,
-    hobbies: ['Programming', 'Photography', 'Creation'],
-    role: Role.ADMIN
-  };
-
-  // Typescript wont catch this as an error at compilation time. (Possibly breaking)
-  // person.role.push('admin');
-
-  // Typescript will catch errors on the following lines
-  // person.role[1] = 10;
-  // person.role = [0, 'admin', 'user'];
-  
-  let favouriteLanguages: string[]; 
-  favouriteLanguages = ['JavaScript', 'Java', 'TypeScript!']
-
-  // Any data type allows for any type to be assigned to that variable.
-  let anyType : any[];
-  anyType = [0, [1, 2, 3], 'anyanyany', true, { id: 0}]
-
-  // Any data type should be avoided. only in very specific cases, such as you don't know what kind of data will be stored.
-  // Hence we should use run time checks instead. such as If statements to check typeof data
-
-
-  console.log(person.name);
-
-  for(const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase()); // type inference of hobby so we can called string methods without errors
-    // console.log(hobby.map()); // ERROR: cannot call map() on a string value. Typescript infers the type and makes this check.
+// | symbolises union types
+const combine = (input1: number | string, input2: number | string) => {
+  let result;
+  if(typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
   }
+  return result; 
+};
 
-  if (person.role === Role.ADMIN) {
-    console.log('user is logged in as a Admin');
-  }
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('joseph', 'kimo');
+console.log(combinedNames);
