65e21f61275b15feb96c46b5b18e3d637aa16eec
Kimo <jm865@kent.ac.uk>
2019-12-19 12:47:04 +0000

    Custom Types

--- app.ts
+++ app.ts
@@ -1,7 +1,18 @@
+// Type Aliases / Custom Types
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+ 
 // | symbolises union types. 'as-number' symbolises a literal type
-const combine = (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') => {
+const combine = (
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) => {
   let result;
-  if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
     result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
@@ -16,11 +27,11 @@ const combine = (input1: number | string, input2: number | string, resultConvers
   // }
 };
 
-const combinedAges = combine(30, 26, 'as-number');
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine('joseph', 'kimo', 'as-text');
+const combinedNames = combine("joseph", "kimo", "as-text");
 console.log(combinedNames);
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges)
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
