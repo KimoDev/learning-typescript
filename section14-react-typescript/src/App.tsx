97de1c2753019a8cf66c9f87be98eb0513b1ee41
Kimo <jm865@kent.ac.uk>
2019-12-20 15:44:45 +0000

    Added PropTypes to Component

--- section14-react-typescript/src/App.tsx
+++ section14-react-typescript/src/App.tsx
@@ -5,9 +5,11 @@ import TodoList from './components/TodoList';
 // A contsant variable that stores/executes a function.
 // React.FC is a custom type that is a Functional Component interface. Also have has ClassicalComponet for classes.
 const App: React.FC = () => {
+  const todos =[{id: 't1', text: 'Finish the course'}]
+
   return (
     <div className="App">
-      <TodoList />
+      <TodoList items={todos} />
     </div>
   );
 }
--- section14-react-typescript/src/components/TodoList.tsx
+++ section14-react-typescript/src/components/TodoList.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const TodoList: React.FC = () => {
-  const todos =[{id: 't1', text: 'Finish the course'}]
+// without a generic interface(propTypes) the component would not see the props passed to it from App.tsx.
+interface propTypes {
+  items: {id: string, text: string}[];
+}
+
+// React.FC is a generic type. Allowing us to define all extra props to this component. (besides default children prop)
+// @propTypes - An interface to define our extra props and their types. 
+const TodoList: React.FC<propTypes> = props => {
   return <ul>
-    {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+    {props.items.map(todo => <li key={todo.id}>{todo.text}</li>)}
   </ul>
 };
 
