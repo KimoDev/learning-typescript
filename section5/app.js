c04cf28121c7a0affeb1c095b80fbb5ffd3ed86c
Kimo <jm865@kent.ac.uk>
2019-12-20 12:59:12 +0000

    Optionals ? - Properties & Parameters

--- section5/app.js
+++ section5/app.js
@@ -1,17 +1,30 @@
+var add;
+add = function (n1, n2) { return n1 + n2; };
+// This class MUST adhear to the contract/structure of the interface/s. 
 // You can implement multiple interfaces. Unlike inheritance.
-// This class MUST adhear to the contract/structure of the interface. 
 var Person = /** @class */ (function () {
     function Person(name) {
         this.age = 30;
-        this.name = name;
+        if (name) {
+            this.name = name;
+        }
     }
     Person.prototype.greet = function (phrase) {
-        console.log(phrase + ' ' + this.name);
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        }
+        else {
+            console.log('Hi!');
+        }
     };
     return Person;
 }());
 var user1;
+// user1.name = 'Kimo'; Error as name cannot be modified after being set due to readonly modifier on the property.
 // valid object as it satisifes the properties of the interface
-user1 = new Person('Joseph');
+user1 = new Person();
+// is Valid. Due to name being optional both as a property & as a constructor parameter.
+// let user2: Greetable;
+// user2 = new Person();
 user1.greet('Hi there - i am');
 console.log(user1);
--- section5/app.ts
+++ section5/app.ts
@@ -6,7 +6,9 @@ interface Greetable extends Named {
 
 // You can add readonly modifier.. But not Public or Private modifiers.
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  // ? symbolises that this property is optional and doesnt need to exist.
+  outputName?: string;
 }
 
 // recap of function types
@@ -25,22 +27,30 @@ add = (n1: number, n2: number) => n1 + n2;
 // This class MUST adhear to the contract/structure of the interface/s. 
 // You can implement multiple interfaces. Unlike inheritance.
 class Person implements Greetable {
-  name : string;
+  name? : string;
   age = 30;
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(name?: string) {
+    if (name) {
+      this.name = name;
+    }
   }
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if(this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
-
 let user1: Greetable;
 // user1.name = 'Kimo'; Error as name cannot be modified after being set due to readonly modifier on the property.
 
 // valid object as it satisifes the properties of the interface
 user1 = new Person('Joseph');
+// is Valid. Due to name being optional both as a property & as a constructor parameter.
+// let user2: Greetable;
+// user2 = new Person();
 
 user1.greet('Hi there - i am');
 console.log(user1);
