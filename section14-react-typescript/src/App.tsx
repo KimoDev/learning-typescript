c7817a6ad40d0322fff2ccf0af71cac29c0094bf
Kimo <jm865@kent.ac.uk>
2019-12-20 16:39:36 +0000

    Added useState Hook with Generic type

--- section14-react-typescript/src/App.tsx
+++ section14-react-typescript/src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 
+import { Todo } from './todo.model';
+
 // A contsant variable that stores/executes a function.
 // React.FC is a custom type that is a Functional Component interface. Also have has ClassicalComponet for classes.
 const App: React.FC = () => {
-  const todos =[{id: 't1', text: 'Finish the course'}]
-
+  // use a state of type - array of objects. These objects are defined by interface types
+  const [todos, addTodo] = useState<Todo[]>([]);
+  console.log(todos);
   // pointer function passed down as props to NewTodo component.
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    // Error if just useState([]). Generics must be applied too. as we have done above with todo.model.js
+    addTodo([{id: Math.random().toString(), text: text}]); 
+    
   }
 
   return (
