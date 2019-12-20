9c46701f40de557503c3be5d20faa3775414630b
Kimo <jm865@kent.ac.uk>
2019-12-20 15:20:58 +0000

    Added TodoList Component to App

--- section14-react-typescript/src/App.tsx
+++ section14-react-typescript/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+
+// A contsant variable that stores/executes a function.
+// React.FC is a custom type that is a Functional Component interface. Also have has ClassicalComponet for classes.
 const App: React.FC = () => {
   return (
-    <>
-
-    </>
+    <div className="App">
+      <TodoList />
+    </div>
   );
 }
 
