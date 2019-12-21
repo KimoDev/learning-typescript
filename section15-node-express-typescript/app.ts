bb4ba683779cf30561a80e6e01274b41d6f1c312
Kimo <jm865@kent.ac.uk>
2019-12-21 03:55:57 +0000

    Added Project Dependacies

--- section15-node-express-typescript/app.ts
+++
@@ -1,9 +0,0 @@
-// Node cannot compile ts files that have ts features like type.
-// However, node can compile ts files that have plain js inside them.
-// ts-node is a package allowing you to compile/execute ts files with node. (development only)
-
-let age: number;
-
-age = 30;
-
-console.log(age);
