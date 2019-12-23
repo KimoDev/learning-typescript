24d485187247bbca5ab1a89e33eec68267a1d43b
Kimo <jm865@kent.ac.uk>
2019-12-23 12:59:32 +0000

    updated tsconfig to compile to ES6+ Javascript

--- section3-typescript-compiler/app.js
+++ section3-typescript-compiler/app.js
@@ -1,3 +1,5 @@
 "use strict";
-var username = 'kimo';
+const username = 'kimo';
+let age;
+age = 30;
 console.log(username);
--- section3-typescript-compiler/app.ts
+++ section3-typescript-compiler/app.ts
@@ -1,3 +1,4 @@
 const username = 'kimo';
-
+let age : number;
+age = 30;
 console.log(username);
