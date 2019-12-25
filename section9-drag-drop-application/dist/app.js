2778ca6143c3eabd089e5217956dbdb52b10cc21
Kimo <jm865@kent.ac.uk>
2019-12-25 17:02:35 +0000

    Added the Rendering of the Form-input to the dom

--- section9-drag-drop-application/dist/app.js
+++ section9-drag-drop-application/dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.domRoot = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.formElement = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.domRoot.insertAdjacentElement('afterbegin', this.formElement);
+    }
+}
+const project = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- section9-drag-drop-application/src/app.ts
+++ section9-drag-drop-application/src/app.ts
@@ -1,1 +1,30 @@
 // Code goes here!
+// This project is written as an object-oriented programming project (OOP)
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement; // Global type from "dom" library in tsconfig.json
+  domRoot: HTMLDivElement;
+  formElement: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; // getElementByiD doesnt know the type we get 
+    this.domRoot = document.getElementById('app')! as HTMLDivElement; // so we cast as a HTMLDivElement
+  
+    // importNode global to the doc object. content is a property existing on a HTMLTemplateElement
+    // Document Fragment
+    const importedNode = document.importNode(this.templateElement.content, true);// true so the Nodes children are also referenced too. (deep clone)
+    // We need the concrete HTML. We use Form because it is the first element in our template
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    // Then add to the dom.
+    this.attach();
+  }
+
+
+  private attach() {
+    // @'afterbegin' - 1 of 4 position options.
+  this.domRoot.insertAdjacentElement('afterbegin', this.formElement)   
+  }
+
+}
+
+const project = new ProjectInput();
